c22169e26807bd9649d8d1bf38a6f75f55a56851
danh22826 <nguyenducanh228k6@gmail.com>
2026-01-06 10:29:00 +0700

    Time: 0 ms (100%), Space: 39.2 MB (26.32%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -3,26 +3,20 @@ using namespace std;
 class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
-        long long  sum = 0;
-        int minVal = INT_MAX;   
+        long long sum = 0;
+        int minVal = INT_MAX;
         int countNegative = 0;
         for(const auto& row : matrix)
         {
             for(int x : row)
             {
-                sum +=(long long)llabs(x);
-                if(x < 0)
-                {
-                    countNegative++;
-                }
-                minVal = min(minVal,abs(x));
-
+                if(x < 0) countNegative++;
+                int abv = abs(x);
+                sum += (long long)abv;
+                minVal = min(minVal,abv);
             }
         }
-        if(countNegative % 2 != 0)
-        {
-            sum -= 2LL * minVal;
-        }
+        if(countNegative % 2 != 0) sum -= 2LL * minVal;
         return sum;
     }
 };
