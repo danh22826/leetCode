729f28d68c981b538749c9cd7bb2202b0c27e724
danh22826 <nguyenducanh228k6@gmail.com>
2026-01-17 15:23:52 +0700

    Time: 1020 ms (61.44%), Space: 250.2 MB (92.81%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
+    vector<int> getEdge(vector<int>& fences, int border)
+    {
+        fences.push_back(1);
+        fences.push_back(border);
+        sort(fences.begin(),fences.end());
+        int size = fences.size();
+        vector<int> edges;
+        for(int i = 0 ; i < size-1; i++)
+        {
+            for(int j = i + 1; j < size; j++)
+            {
+                edges.push_back(fences[j] - fences[i]);
+            }
+
+        }
+        return edges;
+    }
     int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
         const int MOD = 1e9 + 7;
-
-        vector<int> h = getEdges(hFences, m);
-        vector<int> v = getEdges(vFences, n);
-
-        sort(h.begin(), h.end(), greater<int>());
-        sort(v.begin(), v.end(), greater<int>());
-
+        vector<int> h = getEdge(hFences,m);
+        vector<int> v = getEdge(vFences,n);
+        long long result = -1;
+        sort(h.begin(),h.end(),greater<int>());
+        sort(v.begin(),v.end(),greater<int>());
         int i = 0, j = 0;
-        while (i < h.size() && j < v.size()) {
-            if (h[i] == v[j]) {
-                long long x = h[i];
-                return (x * x) % MOD;
+        while(i < h.size() && j < v.size())
+        {
+            if(h[i] == v[j])
+            {
+                result = h[i];
+                return (result * result) % MOD;
             }
-            if (h[i] > v[j]) i++;
+            else if(h[i] > v[j]) i++;
             else j++;
         }
         return -1;
-    }
-
-    vector<int> getEdges(vector<int> fences, int border) {
-        fences.push_back(1);
-        fences.push_back(border);
-        sort(fences.begin(), fences.end());
-
-        vector<int> edges;
-        int sz = fences.size();
-        for (int i = 0; i < sz; i++)
-            for (int j = i + 1; j < sz; j++)
-                edges.push_back(fences[j] - fences[i]);
-
-        return edges;
     }
 };
